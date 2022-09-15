306bcb9d03dae60a2c695d41054eb82a9c8385e2
Radosław Dziadosz <radoslaw@dziadosz.eu>
2022-09-15 22:15:10 +0200

    PR-01: Add Flink Connector end-to-end tests framework - project skeleton (#435)
    Co-authored-by: Radosław Dziadosz <radoslaw.dziadosz@getindata.com>

---
+++ flink/end-to-end-tests/terraform/outputs.tf
@@ -0,0 +1,33 @@
+output "eks_cluster_name" {
+  value = "It will be implemented in the PR-02-A"
+}
+
+output "eks_cluster_endpoint" {
+  value = "It will be implemented in the PR-02-A"
+}
+
+output "account_id" {
+  value = "It will be implemented in the PR-02-A"
+}
+
+output "region" {
+  value = "It will be implemented in the PR-02-A"
+}
+
+output "service_account_role_name" {
+  value = "It will be implemented in the PR-02-A"
+}
+
+output "secret_key" {
+  value     = "It will be implemented in the PR-02-A"
+  sensitive = true
+}
+
+output "access_key" {
+  value     = "It will be implemented in the PR-02-A"
+  sensitive = true
+}
+
+output "test_data_bucket_name" {
+  value = "It will be implemented in the PR-02-A"
+}
